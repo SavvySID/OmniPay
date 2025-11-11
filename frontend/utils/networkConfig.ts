65029309ab1884d4fd013aad944451f343340782
SavvySID <siddhant3187@gmail.com>
2025-11-11 18:43:28 +0530

    chore(frontend): tweak network config

--- frontend/utils/networkConfig.ts
+++ frontend/utils/networkConfig.ts
@@ -1,6 +1,6 @@
 export const config = {
   backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000",
-  polygonMumbaiChainId: 80001,
+  polygonAmoyChainId: 80002, // Polygon Amoy testnet (Mumbai was deprecated)
 };
 
 
